e60ceb6fc11bbfb85c9c885e2a5fc61f8018893b
Christoph Bünte <info@christophbuente.de>
2014-09-09 21:18:04 +0200

    Remove obsolete stuff from seed.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -205,11 +205,3 @@ node_types = NodeType.create([
   {:category => health,  :identifier => :social_facility,   :osm_key => 'amenity',  :osm_value => 'social_facility',  :icon => 'social_facility.png'},
   {:category => health,  :identifier => :dentist,           :osm_key => 'amenity',  :osm_value => 'dentist',          :icon => 'dentist.png'}
 ])
-
-Region.delete_all
-Region.connection.execute('ALTER TABLE regions AUTO_INCREMENT=1')
-
-f = File.new "#{Rails.root}/db/data/polygons.sql"
-while statements = f.gets("\n") do
-  ActiveRecord::Base.connection.execute(statements)
-end
